b0d792d700be5593e2dcffa0b834632bf499949b
Grzegorz Haftarczyk <grzegorz.haftarczyk@gmail.com>
2022-10-16 12:03:39 +0200

    update game logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {    
 
     public GameObject prefabBody;
+    public GameObject prefabFruit;
+    public GameObject environment;
+    public float boardLeft, boardRight, boardUp, boardDown;
     float speed = 0.4F;
     enum Direction { LEFT, RIGHT, TOP, BOTTOM }
     Direction direction = Direction.LEFT;
     Vector3Int[] directionVector = {new Vector3Int(-1, 0, 0), new Vector3Int(1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(0, -1, 0)};
-    // Start is called before the first frame update
+    
+    private float horizontalLength, vertivalLength;
     enum State {
         MOVE,
         EAT,
@@ -19,22 +22,15 @@ public class Player : MonoBehaviour
     State gameState = State.MOVE;
     void Start()
     {
-        // TODO think about colors: Head - blue, Body - green, Tail - red?
-        int children = transform.childCount;
-        print("count: "+children);
-        for (int i = 0; i < children; ++i)
-        {
-            print("For loop: " + transform.GetChild(i).gameObject.name);
-        }
-        StartCoroutine(move()); 
+        horizontalLength = (int)((boardRight - boardLeft)/speed);
+        vertivalLength = (int)((boardUp - boardDown)/speed);
+        addRandomFruit();
+        StartCoroutine(move());
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        //Get the value of the Horizontal input axis.
-
         float verticalInput = Input.GetAxis("Vertical");
         if(horizontalInput > 0.1) {
             direction = Direction.RIGHT;
@@ -58,12 +54,10 @@ public class Player : MonoBehaviour
             Vector3 jump = new Vector3(currentDirection.x * speed, currentDirection.y * speed, 0);
             Vector3 current = transform.GetChild(0).position;
             RaycastHit hitInfo;
-            // Debug.DrawRay(current, current + jump * speed / 2, Color.green, 1, false);
             if (prevState == State.MOVE) 
             {
                 transform.GetChild(transform.childCount - 1).gameObject.SetActive(false);
             }
-            Debug.DrawRay(current, currentDirection * speed, Color.green, 1, false);
             if (Physics.Raycast(current, currentDirection, out hitInfo, speed))
             {
                 gameState = collideWith(hitInfo.collider);
@@ -77,16 +71,8 @@ public class Player : MonoBehaviour
             Vector3 newPosition = current + jump;
             transform.GetChild(0).position = transform.GetChild(0).position + jump;
             Vector3 nextPosition = current;
-            // If end the game, then end the game
-            // If obstacle body, then end the game
-            // If tail and privies move was eat, then end game
-            // If tail and privies move was move, then regular move
-            // move after eat: move all (without tail), then add last body part (tail stay in the same place, use Transform.SetAsLastSibling on a tail)
-            // regular move: move all, with tail
             if(prevState == State.EAT)
             {
-                // TODO put new body after head
-                // rest of body stay in the same place
                 GameObject second = Instantiate(prefabBody, nextPosition, Quaternion.identity);
                 second.name = "Body";
                 second.transform.parent = transform;
@@ -94,14 +80,12 @@ public class Player : MonoBehaviour
             }
             if (prevState == State.MOVE)
             {
-                Debug.Log("move body");
                 for (int i = 1; i < transform.childCount - 1; i++)
                 {
                     current = transform.GetChild(i).position;
                     transform.GetChild(i).position = nextPosition;
                     nextPosition = current;
                 }
-                Debug.Log("move rest of the tail");
                 transform.GetChild(transform.childCount - 1).position = nextPosition;
                 transform.GetChild(transform.childCount - 1).gameObject.SetActive(true);
             }
@@ -118,11 +102,32 @@ public class Player : MonoBehaviour
         } else if (coolider.tag == "Food") {
            Debug.Log("Yummy!!!");
            Destroy(coolider.gameObject);
+           addRandomFruit();
            return State.EAT;
         } else {
            Debug.Log("Error!!!"); 
         }
         return State.MOVE;
     }
+
+    private void addRandomFruit() {
+        Vector3 randomPlaceOnBoard = getRandomVectorOnBoard();
+        while(Physics.OverlapBox(randomPlaceOnBoard, new Vector3(0.075f, 0.075f, 0.075f)).Length > 1)
+        {
+            randomPlaceOnBoard = getRandomVectorOnBoard();
+        }
+        addFruit(randomPlaceOnBoard);
+    }
+
+    private void addFruit(Vector3 randomPlaceOnBoard) {
+        GameObject fruit = Instantiate(prefabFruit, randomPlaceOnBoard, Quaternion.identity);
+        fruit.transform.parent = environment.transform;
+    }
+
+    private Vector3 getRandomVectorOnBoard()
+    {
+        return new Vector3(Random.Range(0, horizontalLength) * speed - boardRight, Random.Range(0, vertivalLength) * speed - boardUp, 0);
+    }
+
 
 }
